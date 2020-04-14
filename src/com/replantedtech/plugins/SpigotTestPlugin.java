7df7b986040e1457c8588c28d0f421c6ce00ad85
DrRedPanda <plantstevie@gmail.com>
2020-04-13 22:20:40 -0400

    Upload Code

---
+++ src/com/replantedtech/plugins/SpigotTestPlugin.java
@@ -0,0 +1,15 @@
+package com.replantedtech.plugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotTestPlugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
